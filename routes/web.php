4f764631c2c0824849a8afe7fab46656621cac0e
Diego Caucota <diegocaucota@gmail.com>
2024-09-12 11:04:15 -0300

    add: controllers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Importar el archivo HomeController desde Http/Controllers
+use App\Http\Controllers\HomeController;
+
+// Pasamos 2 parámetros: la clase y el método
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/posts/create', function () {
+    return "Aqui se mostrará un formulario para crear un post";
 });
+
+Route::get('/posts/{post}', function($post) {
+    return "Aqui se mostrará el post {$post}";
+});
+
+
