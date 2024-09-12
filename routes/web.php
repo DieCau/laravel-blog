145631ab0f3f27961906c667e9090a6fe6e04088
Diego Caucota <diegocaucota@gmail.com>
2024-09-12 11:20:57 -0300

    add: PostController

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 // Importar el archivo HomeController desde Http/Controllers
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+
+// Cuando solo tenemos __invoke en el metodo NO hace falta pasar el nombre como tal
+Route::get('/', [HomeController::class]);
 
 // Pasamos 2 parámetros: la clase y el método
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('/posts/create', function () {
-    return "Aqui se mostrará un formulario para crear un post";
-});
+Route::get('/posts/create', [PostController::class, 'create']);
 
-Route::get('/posts/{post}', function($post) {
-    return "Aqui se mostrará el post {$post}";
-});
+Route::get('/posts/{post}', [PostController::class, 'show']);
 
 
