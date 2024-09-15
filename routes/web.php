1cdec40984c947bafaf33c966f6a658ba3e0b146
Diego Caucota <diegocaucota@gmail.com>
2024-09-14 23:04:46 -0300

    fix: migrations

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 // Importar el archivo HomeController desde Http/Controllers
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 
 // Cuando solo tenemos __invoke en el metodo NO hace falta pasar el nombre como tal, sin los [ ]
 Route::get('/', HomeController::class);
@@ -15,5 +16,22 @@ Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 
 Route::get('/posts/{post}', [PostController::class, 'show']);
+
+Route::get('prueba', function() {
+    // Creo una instancia del modelo Post y lo asigno en var $post
+    $post = new Post;
+
+    // Asignamos los campos al registro nuevo
+    $post->title = 'Titulo de prueba 1';
+    $post->content = 'Contenido de prueba 1';
+    $post->category = 'Categoria de prueba 1';
+
+    // Guardar los datos
+    $post->save();
+
+    return $post;
+});
+
+
 
 
